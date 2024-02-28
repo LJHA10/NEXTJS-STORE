b040407fe12d62eecdca96fabd3ecdaffff5f051
Luis Javier <luisjavierhernandez150@gmail.com>
2024-02-28 17:21:41 -0600

    commit 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1> Hola mundo</h1>
-    </main>
-  );
+
+
+export default function Home(){
+
+    return (
+        <main>
+
+            <h1>Hola mundo</h1>
+
+        </main>
+    )
 }
