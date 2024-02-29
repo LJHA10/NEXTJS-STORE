5b19aa603fc37c4d8747e25556581859d2a0d72f
Luis Javier <luisjavierhernandez150@gmail.com>
2024-02-29 16:37:36 -0600

    commit 2_1CSSModulesEnNext

--- src/componentes/home/Hero/Hero.tsx
+++ src/componentes/home/Hero/Hero.tsx
@@ -1,10 +1,17 @@
+
+import styles from './Hero.module.css' 
+
+
 export const Hero = () => {
+
 
     return(
 
-        <section>
+        <section className={styles.Hero}>
         
-        <h1>Hero</h1>
+        <h1>Future world</h1>
+
+        <h2>Empowering your tomorrot, today!</h2>
 
         </section>
     )
