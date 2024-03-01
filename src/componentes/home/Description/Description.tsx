732eb512fb72c6f7714cbda9958331877a82297c
Luis Javier <luisjavierhernandez150@gmail.com>
2024-02-29 19:00:34 -0600

    commit 2_5ManejoyOptimizacionDEImagenes

--- src/componentes/home/Description/Description.tsx
+++ src/componentes/home/Description/Description.tsx
@@ -1,15 +1,33 @@
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Description = () => {
 
     return(
 
-        <section>
+        <section className={styles.Description}>
 
-            <img src="/images/tecnologia.JPG" alt="products marketplace" />
-            <h2>Description</h2>
+            <img 
+            
+            src="/images/tecnologia.JPG" 
+            
+            alt="products marketplace" 
+            
+            width={500} 
+            
+            height={300} 
 
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, officia voluptas saepe odio fuga vel iste fugiat repellat eos dignissimos sed quaerat amet totam a, sit, nobis provident similique quos.</p>
+            />
 
+            <div className={styles.Description__text}>
+
+            <h2>Bring the future today</h2>
+
+            <p>Future World: Your Gateway to Tomorrow's Tech! Drive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+
+            </div>
 
         </section>
     )
 }
+
