99a0bf1e784d0d7d4bc13f8b59a97a4c31b09ce3
Luis Javier <luisjavierhernandez150@gmail.com>
2024-02-29 18:18:56 -0600

    commit 2_4ComoAgregarArchivosEstaticos

--- src/componentes/home/Description/Description.tsx
+++ src/componentes/home/Description/Description.tsx
@@ -3,8 +3,13 @@ export const Description = () => {
     return(
 
         <section>
-        
-        <h1>Descripción</h1>
+
+            <img src="/images/tecnologia.JPG" alt="products marketplace" />
+            <h2>Description</h2>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, officia voluptas saepe odio fuga vel iste fugiat repellat eos dignissimos sed quaerat amet totam a, sit, nobis provident similique quos.</p>
+
+
         </section>
     )
 }
