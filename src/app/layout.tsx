9af2c5b8619a90221232d6a11f838145a3639558
Luis Javier <luisjavierhernandez150@gmail.com>
2024-02-29 20:52:48 -0600

    commit 2_7OptimizacionDeFuentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 
-import { Inter } from "next/font/google";
+import {Roboto} from 'next/font/google'
+
 import { Header } from "app/componentes/shared/Header";
 import { Footer } from "app/componentes/shared/Footer";
 import 'app/sass/global.sass'
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["100", "300","500","700"],
+  subsets: ["latin"]
+})
 
 export default function RootLayout({
   children,
@@ -15,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={inter.className}>
+      <body className={roboto.className}>
       
       <Header/>
 
