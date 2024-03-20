360677f70f38616aec92f1a34e31922acf143ac9
Luis Javier <luisjavierhernandez150@gmail.com>
2024-03-20 12:54:58 -0600

    commit 4_7PatronesdedataFetchin

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,20 +1,27 @@
+import { ProductsWrapper } from "app/componentes/Store/ProductsWrapper"
+import { getCollections } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify/products"
+
+
 import React from "react"
 
 interface CategoryProps {
     params: {
         categories: string [],
+    }
         
         searchParams?: string
     }
-}
 
-export default function Category(props : CategoryProps){
+export default async function Category(props : CategoryProps){
     
+    const products = await getProducts()
+
     const {categories} = props.params
 
     // throw new Error('Error: Boom!!')
 
     return ( 
-        <h1>categoria dinamica: {categories}</h1>
+        <ProductsWrapper products={products}/>
     )
 }
--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -2,18 +2,31 @@ import { Hero } from "app/componentes/home/Hero/Hero"
 import { Description } from "app/componentes/home/Description/Description"
 import { Header } from "app/componentes/shared/Header"
 import { Footer } from "app/componentes/shared/Footer"
+import { getCollections } from "app/services/shopify/collections"
+import Link from "next/link"
 
-export default function Layout({children}: {children: React.ReactNode}){
-
+export default async function Layout({children}: {children: React.ReactNode}){
+    const collections = await getCollections()
+   
+   
     return(
-        <div>
+        <main>
+            <nav>
+                {
+                    collections.map((collection) => (
+                        <Link key={collection.id} href={'/store/' + collection.handle}>
+                        {collection.title}
+                        
+                        </Link>
+                    ))
+                }
 
-            <Hero />
 
-            <Description />
+            </nav>
 
-            {children}
 
-        </div>
+
+
+        </main>
     )
 }
