a32d494cde5081099aabdc52e36cb3d97a004e26
Luis Javier <luisjavierhernandez150@gmail.com>
2024-04-17 15:52:15 -0600

    commit 4_13ComoHacerRedirect

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,9 @@
+"use client"
 import { ProductView } from "app/componentes/product/ProductView/ProductView"
 import { getProducts } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
+import { useRouter } from "next/router"
+
 
 interface ProductPageProps {
     searchParams:{
@@ -14,5 +18,9 @@ export default async function Productpage({searchParams}: ProductPageProps){
 
     const product = products[0]
 
+    if(!id){
+        redirect('/store')
+    }
+    
     return <ProductView  product={product}/>
 }
